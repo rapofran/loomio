c09af203b0e59111ed160893ff19768716eb8b12
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-06-15 13:03:57 -0300

    permitir reiniciar passenger y delayed jobs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'loomio'
 set :repo_url, 'https://github.com/piratas-ar/loomio'
 set :linked_files, %w{.env}
-set :linked_dirs, %w{public/system}
+set :linked_dirs, %w{public/system log tmp/pids}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.1'
