7049afa1fb6b6253d1b5e7037a76580b444ef55d
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-07-20 19:08:05 -0300

    no google

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624024541) do
+ActiveRecord::Schema.define(version: 20150709164011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-  #enable_extension "pg_stat_statements"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "resource_id",   limit: 255, null: false
@@ -661,11 +660,6 @@ ActiveRecord::Schema.define(version: 20150624024541) do
     t.string   "reset_password_token",             limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                                default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",               limit: 255
-    t.string   "last_sign_in_ip",                  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name",                             limit: 255
