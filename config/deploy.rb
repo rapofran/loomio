b4a3d89b408359515a44b2c9925b38589f012950
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-08-09 20:43:41 -0300

    ruby 2.2.2 in rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :linked_files, %w{.env}
 set :linked_dirs, %w{public/system log tmp/pids}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.1'
+set :rbenv_ruby, '2.2.2'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
