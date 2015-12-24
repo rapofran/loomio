7ab11c23de3f732462e407c40a140a03d3e76c7a
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-12-24 14:04:56 -0300

    mostrar nuestra portada

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -3,12 +3,8 @@ class MarketingController < ApplicationController
     if user_signed_in?
       redirect_to dashboard_path
     else
-      if third_party_install?
-        redirect_to new_user_session_path
-      else
-        @stories = BlogStory.order('published_at DESC').limit(4)
-        render layout: false
-      end
+      @stories = BlogStory.order('published_at DESC').limit(4)
+      render layout: false
     end
   end
 end
