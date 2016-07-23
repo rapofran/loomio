aa036c9ebe036a7cfa211d1b33a7ea50515e7226
fauno <fauno@endefensadelsl.org>
2016-07-23 18:27:07 -0300

    la migracion a commited ya puede estar aplicada

--- db/migrate/20160714232929_add_commited_count_to_motion.rb
+++ db/migrate/20160714232929_add_commited_count_to_motion.rb
@@ -1,5 +1,9 @@
 class AddCommitedCountToMotion < ActiveRecord::Migration
   def change
-    add_column :motions, :commited_votes_count, :integer, null: false, default: 0
+    # Estamos trayendo la migración desde otra rama, y algunos deploys
+    # ya la tienen implementada
+    unless column_exists? :motions, :commited_votes_count
+      add_column :motions, :commited_votes_count, :integer, null: false, default: 0
+    end
   end
 end
