8839f124ac5c72b85ecf52180290fa27c548676a
Saico <elsaico@partidopirata.com.ar>
2016-08-12 21:38:33 -0300

    log para delayes jobs

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,3 +1,4 @@
 Delayed::Worker.default_priority = 10
 Delayed::Worker.max_attempts = ENV['MAX_JOB_ATTEMPTS'] || 10
 Delayed::Worker.delay_jobs = Rails.env.production?
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
